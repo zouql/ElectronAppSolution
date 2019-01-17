4ed37addad0d362e8fbbef4934234fb3ac6d58ca
zouql <Thinkxuhao@outlook.com>
2019-01-17 13:11:38 +0800

    整理文件

--- ElectronApp/main.js
+++ ElectronApp/main.js
@@ -1,5 +1,7 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -10,7 +12,14 @@ function createWindow() {
     mainWindow = new BrowserWindow({ width: 1000, height: 800 });
 
     // and load the index.html of the app.
-    mainWindow.loadFile('index.html');
+    //mainWindow.loadFile('index.html');
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
